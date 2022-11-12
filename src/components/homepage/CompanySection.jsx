d644168bbcaf242fab50c4526e557ee7ade3b8ef
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-11-12 14:40:25 +0100

    Made company section responsive

--- src/components/homepage/CompanySection.jsx
+++ src/components/homepage/CompanySection.jsx
@@ -1,3 +1,4 @@
+import Container from "../utils/Container";
 import mbToken from "./../../assets/mb-token.png";
 import metamask from "./../../assets/metamask.png";
 import opensea from "./../../assets/opensea.png";
@@ -9,11 +10,11 @@ import styles from "./../../styles.module.css";
 const CompanySection = () => {
     return (
         <div className={`${styles.company}`}>
-            <div className="flex justify-between px-24 py-3">
-                <div><img src={mbToken} alt={"mb-token"}/></div>
-                <div><img src={metamask} alt={"metamask"}/></div>
-                <div><img src={opensea} alt={"opensea"}/></div>
-            </div>
+                <div className="h-full flex flex-col md:flex-row justify-between py-6 items-center md:justify-between md:px-24 md:py-3">
+                    <div><img src={mbToken} alt={"mb-token"}/></div>
+                    <div><img src={metamask} alt={"metamask"}/></div>
+                    <div><img src={opensea} alt={"opensea"}/></div>
+                </div>
         </div>
     );
 }
