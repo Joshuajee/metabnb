856f953d4256df427c3e1ea91ab2262efc2462c2
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-11-12 16:01:32 +0100

    feat: made call to action responsive

--- src/components/homepage/CallToAction.jsx
+++ src/components/homepage/CallToAction.jsx
@@ -1,3 +1,4 @@
+import Container from "../utils/Container";
 import ctaImage from "./../../assets/cta-image.png";
 import styles from "./../../styles.module.css";
 
@@ -5,17 +6,17 @@ import styles from "./../../styles.module.css";
 
 const CallToAction = () => {
     return (
-        <div className={`${styles.cta} flex justify-center`}>
+        <div className={`${styles.cta}`}>
 
-            <div className="max-w-screen-2xl">
+            <Container>
 
-                <div className="flex items-center m-28">
+                <div className="my-10 lg:my-20 flex flex-col sm:flex-row items-center justify-center content-center h-full">
 
-                    <div className="basis-2/5 items-center contents-center">
+                    <div className="basis-full sm:basis-2/5 items-center contents-center">
 
-                        <h2 className={``}>Metabnb NFTs</h2>
+                        <h2>Metabnb NFTs</h2>
 
-                        <p className="font-normal text-2xl mt-14">
+                        <p className="font-normal sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-4 lg:mt-14">
                             Discover our NFT gift cards collection. 
                             Loyal customers gets amazing gift cards
                             which are traded as NFTs. These NFTs 
@@ -23,19 +24,16 @@ const CallToAction = () => {
                             our exclusive services.
                         </p>
 
-                        <button className="px-10 py-3 mt-14 text-base"> Learn more </button>
-
+                        <button className="px-10 py-3 mt-4 lg:mt-14 text-base"> Learn more </button>
                     </div>
 
-                    <div className="basis-3/5">
-
+                    <div className="basis-full sm:basis-3/5 mt-10 md:mt-0">
                         <img src={ctaImage} alt="cta" />
-
                     </div>
                 
                 </div>
 
-            </div>
+            </Container>
 
         </div>
     );
